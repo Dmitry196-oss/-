16fc754f3eb5c6414210d9f17ee9347a7518af2a
Dmitry196-oss <dima1705kim@gmail.com>
2026-04-05 15:14:57 +0900

    Add files via upload

---
+++ quiz.js
@@ -0,0 +1,46 @@
+
+
+  
+function petr() {
+    window.location.href = 'quiz1.html'}
+
+
+
+
+
+function kk() {
+    window.location.href = 'quiz.html'
+}
+function hello() {
+    window.location.href ='quiz2.html'
+}
+
+function hello1() {
+    window.location.href ='quiz3.html'
+}
+
+ function kk1() {
+    window.location.href='quiz1.html'
+ }
+ function kk2() {
+    window.location.href='quiz2.html'
+
+ }
+function hello2() {
+    window.location.href='quiz4.html'
+ }
+ function hello3() {
+    window.location.href='quiz5.html'
+
+ }
+ function kk3() {
+    window.location.href='quiz3.html'
+ }
+ function hello4() {
+    window.location.href='quiz4.html'
+ }
+ function kk4() {
+
+ 
+ document.getElementById('message').innerHTML = 'Спасибо! Ваша заявка отправлена. Мы свяжемся с вами в ближайшее время)'
+ }
